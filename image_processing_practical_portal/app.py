d76132d819f95d492d4fd911a9c68df8f63cd794
Sahil Bhiwankar <sahilbhiwankar34@gmail.com>
2026-09-11 19:11:51 +0530

    Modify server run configuration
    Updated server configuration to use environment port and changed host to 0.0.0.0.

--- image_processing_practical_portal/app.py
+++ image_processing_practical_portal/app.py
@@ -1432,14 +1432,18 @@ def uploaded_file(filename):
     )
 
 
+
 # ============================================================
 # RUN SERVER
 # ============================================================
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
